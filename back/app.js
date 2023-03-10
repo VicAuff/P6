2b51b046757c9df8ea35bd848f8249a2cff711e5
victor.a <victor_auffray@orange.fr>
2023-03-10 15:33:52 +0100

    Familiarisation générale

--- back/app.js
+++ back/app.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://HelloWorld2:T9rj8yEjzTVhwTbs@cluster0.xefdcja.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée....."));
 
 const app = express();
+
+// Analyse des requêtes json
+app.use(express.json());
 
 // Paramétrage des en-têtes CORS
 app.use((req, res, next) => {
@@ -16,8 +28,36 @@ app.use((req, res, next) => {
   next();
 });
 
-// Analyse des requêtes json
-app.use(express.json());
+app.post("/api/auth/login", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Objet créé !",
+  });
+});
+
+app.get("/api/auth/login", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 4900,
+      userId: "qsomihvqios",
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 2900,
+      userId: "qsomihvqios",
+    },
+  ];
+  res.status(200).json(stuff);
+});
 
 // Export du module app
 module.exports = app;
