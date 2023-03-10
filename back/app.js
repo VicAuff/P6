756247eb7e3427a8176b5b6c07a25c72c4a11efa
victor.a <victor_auffray@orange.fr>
2023-03-10 13:15:28 +0100

    first commit

---
+++ back/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+// Paramétrage des en-têtes CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+// Analyse des requêtes json
+app.use(express.json());
+
+// Export du module app
+module.exports = app;
